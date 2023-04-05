021cde8eca96151b54d2cc63c152eace234aaf6f
Tama <arifnovrian@gmail.com>
2023-04-06 00:45:22 +0700

    update json

--- models/userdetail.js
+++ models/userdetail.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      UserDetail.hasOne(models.User, { foreignKey: "UserDetailId" })
     }
   }
   UserDetail.init({
